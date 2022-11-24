469e67d466fe308deab9ee3b1c9458cdebbc4d28
visawjeet mallik <vishmallik@gmail.com>
2022-11-24 10:20:15 +0530

    add Readme and remove some unused stuffs

---
+++ README.md
@@ -0,0 +1,85 @@
+
+# Trakr - The Expense Tracker App
+
+An expense tracker application made using Express & MongoDB where a logged in user can track their income, expenses and savings.
+
+Each user will have thier own set of income source, expenses category and saving calculated based on income & expenses.
+## Badges
+
+[![MIT License](https://img.shields.io/badge/License-MIT-green.svg)](https://choosealicense.com/licenses/mit/) 
+ 
+[![Express](https://img.shields.io/badge/Framework-Express-orange)](https://expressjs.com/)
+
+[![Database](https://img.shields.io/badge/DB-MongoDB-brightgreen)](https://www.mongodb.com/)
+
+[![Passportjs](https://img.shields.io/badge/OAuth-Passport.js-yellow)](https://www.passportjs.org/)
+
+## Features
+
+- User can create account and login in with the same
+- Social OAuth Login supported (Google and Github)
+- Filter by date, by category , by both date and category, by month and year and so on.
+- User can see their monthly saving.
+
+
+## Screenshots
+
+**Landing Page:**
+![App Screenshot](https://user-images.githubusercontent.com/64186460/203693692-0493efed-0d56-41ea-9a84-261a809db290.png)
+
+**Home Page:**
+![App Screenshot](https://user-images.githubusercontent.com/64186460/203693690-13a8e452-75ca-446e-9d8f-83366c6bef4e.png)
+
+**Dashoard:**
+![Screenshot (4)](https://user-images.githubusercontent.com/64186460/203693688-a3269925-0fc5-4987-995b-aa968b3a9682.png)
+
+**Filter by month:**
+![Screenshot (3)](https://user-images.githubusercontent.com/64186460/203693694-6f5bf18d-1e5b-43ce-9fe2-dd9d50095440.png)
+
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://github.com/vishmallik/Expense-Tracker.git
+```
+
+Go to the project directory
+
+```bash
+  cd Expense-Tracker
+```
+
+Install dependencies
+
+```bash
+  npm install
+```
+
+Start the server
+
+```bash
+  npm run start
+```
+
+
+## Demo
+
+https://expense-tracker-fbdd.onrender.com
+
+
+## Authors
+
+- [@vishmallik](https://www.github.com/vishmallik)
+
+
+## Feedback
+
+If you have any feedback, please reach out to me at vishmallik@gmail.com
+
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+
--- routes/users.js
+++ routes/users.js
@@ -1,45 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
-const nodemailer = require("nodemailer");
-const crypto = require("crypto");
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    type: "OAUTH2",
-    user: process.env.ID,
-    clientId: process.env.CLIENT_ID_GOOGLE,
-    clientSecret: process.env.CLIENT_SECRET_GOOGLE,
-    refreshToken: process.env.REFRESH_TOKEN,
-    accessToken: process.env.ACCESS_TOKEN,
-  },
-  tls: {
-    rejectUnauthorized: false,
-  },
-});
-router.get("/verify-email", (req, res, next) => {
-  let token = req.query.token;
-  User.findOne({ emailToken: token }, (err, user) => {
-    if (err) {
-      console.log("email not verified");
-      return res.redirect("/");
-    }
-    User.findByIdAndUpdate(
-      user.id,
-      { emailToken: null, isVerified: true },
-      (err, user) => {
-        if (err) {
-          return next(err);
-        }
-        return res.redirect("/login");
-      }
-    );
-  });
-});
 router.post("/register", (req, res, next) => {
-  let { email, password, name } = req.body;
-  req.body.emailToken = crypto.randomBytes(64).toString("hex");
+  let { email, password } = req.body;
   if (!email && !password) {
     req.flash("error", "Email/password is required");
     return res.redirect("/");
@@ -56,21 +20,6 @@ router.post("/register", (req, res, next) => {
       }
       return next(err);
     }
-    const mailOptions = {
-      from: '"Verify Your Email" <vishmallik@gmail.com> ',
-      to: user.email,
-      subject: "Trakr - Verify Your Email",
-      html: `<h2>${user.name}! Thanks for registering on Trakr!!!</h2>
-              <h4>Please verify your email to continue...</h4>
-              <a href="http://${req.headers.host}/users/verify-email?token=${user.emailToken}">Verify Your Email</a>`,
-    };
-    transporter.sendMail(mailOptions, (err, info) => {
-      if (err) {
-        console.log(err);
-      } else {
-        console.log("verification mail is sent");
-      }
-    });
     req.flash("success", "User registered successfully");
     return res.redirect("/login");
   });
@@ -104,10 +53,6 @@ router.post("/login", (req, res, next) => {
       return res.redirect("/home");
     });
   });
-});
-
-router.get("/password-reset", (req, res, next) => {
-  //to be added with nodemailer
 });
 router.get("/logout", (req, res, next) => {
   req.session.destroy();
